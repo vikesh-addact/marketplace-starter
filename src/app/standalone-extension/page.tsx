23ee5641b222f5f80a184c3d8a507fe1e5089fe6
uttam-vml <142833242+vikesh-addact@users.noreply.github.com>
2026-06-25 19:02:35 +0530

    Revert "Update page.tsx"
    This reverts commit 4589441e3cdfccd72acea5b2deff8532873327f3.

--- src/app/standalone-extension/page.tsx
+++ src/app/standalone-extension/page.tsx
@@ -444,7 +444,7 @@ function StandaloneExtension() {
                 setIsLoadingMedia(true);
 
                 const pageSize = 200;
-                let skip = 0;
+                let page = 1;
                 let totalCount = 0;
                 let allResults: Array<Record<string, unknown>> = [];
 
@@ -459,7 +459,7 @@ function StandaloneExtension() {
                                 query: {
                                 index: "sitecore_master_index"
                                 latestVersionOnly: true
-                                paging: { pageSize: ${pageSize}, skip: ${skip} }
+                                paging: { pageSize: ${pageSize}, page: ${page} }
                                 searchStatement: {
                                     criteria: [
                                     { field: "_path", value: "90ae357f61714ea9808c5600b678f726", criteriaType: "EXACT", operator: "MUST" }
@@ -523,7 +523,7 @@ function StandaloneExtension() {
 
                     totalCount = pageData.totalCount ?? 0;
                     allResults = allResults.concat(pageData.results);
-                    skip += pageSize;
+                    page++;
                 } while (allResults.length < totalCount);
 
                 const allMediaPayload = { data: { search: { results: allResults } } };
